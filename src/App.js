1989dba6a5229b7a33ca8b0e506a885f590b1978
xharris <xhh950@gmail.com>
2019-10-03 00:09:54 -0400

    initial setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//@flow
+import React from 'react';
+import './App.css';
+
+import './features/ingredient';
+
+function App() {
+  return (
+    <div className="App">
+      test
+    </div>
+  );
+}
+
+export default App;
